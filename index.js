d9dd47e5dea8f6cebb84af6870d7c66bf3a1e2be
Will Yazdani <yazd0006@gmail.com>
2022-02-26 10:04:05 -0600

    added question structure

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs')
 const generateMarkdown = require('./Develop/generateMarkdown')
-// TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    { //title
+        type: "input",
+        name: "title",
+        message: "Enter your repo name:"
+    },
+
+    { //description
+        type: "input",
+        name: "description",
+        message: "Enter repo description:"
+    },
+
+    { //installation
+        type: "input",
+        name: "installation",
+        message: "Enter instiallation instructions:"
+    },
+
+    { //usage
+        type: "input",
+        name: "usage",
+        message: "Enter usage instructions:"
+    },
+
+    { //contributing
+        type: "input",
+        name: "contributing",
+        message: "Enter contribution instructions:"
+    },
+
+    { //tests
+        type: "input",
+        name: "tests",
+        message: "Enter testing instructions:"
+    },
+
+    { //liscense
+        type: "list",
+        name: "liscense",
+        message: "Choose a liscense:",
+        choices: ["MIT", "Creative Commons", "Apache 2.0", "None"]
+    },
+
+    { //username
+        type: "input",
+        name: "username",
+        message: "Enter your GitHub username:"
+    },
+
+    { //email
+        type: "input",
+        name: "email",
+        message: "Enter your email address:"
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
