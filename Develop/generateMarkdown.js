b6af792cbcffffd0d4847b031a10ecc8ba1201bb
Will Yazdani <yazd0006@gmail.com>
2022-02-26 12:31:29 -0600

    added liscence badge links

--- Develop/generateMarkdown.js
+++ Develop/generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (liscense === 'MIT') {
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    } else if (liscense === 'Creative Commons') {
+        return `[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)`
+    } else if (liscense === 'Apache 2.0') {
+        return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,8 +20,20 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+    const anyname = renderLicenseBadge()
   return `# ${data.title}
+
+  ## Table of Contents
+
+  - [Installation]
+  - [Usage]
+  - [Contributing]
+  - [Tests]
+  - [Questions]
+  
+${anyname}
 `;
+
 }
 
 module.exports = generateMarkdown;
