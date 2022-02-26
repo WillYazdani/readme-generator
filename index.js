38f8bfbad6cef5e31827ae7a203a26e6fd68fb53
Will Yazdani <yazd0006@gmail.com>
2022-02-26 10:43:23 -0600

    user input added to questions

--- index.js
+++ index.js
@@ -1,67 +1,117 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer')
-const fs = require('fs')
-const generateMarkdown = require('./Develop/generateMarkdown')
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./Develop/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = () => {
+    return inquirer.prompt([
 
-    { //title
-        type: "input",
-        name: "title",
-        message: "Enter your repo name:"
-    },
+        { //title
+            type: "input",
+            name: "title",
+            message: "Enter your repo name:",
+            validate: (userInput) => {
+                if (userInput) {
+                    return true
+                }
+                return false;
+            }
+        },
 
-    { //description
-        type: "input",
-        name: "description",
-        message: "Enter repo description:"
-    },
+        { //description
+            type: "input",
+            name: "description",
+            message: "Enter repo description:",
+            validate: (userInput) => {
+                if (userInput) {
+                    return true
+                }
+                return false;
+            }
+        },
+        
+        { //installation
+            type: "input",
+            name: "installation",
+            message: "Enter instiallation instructions:",
+            validate: (userInput) => {
+                if (userInput) {
+                    return true
+                }
+                return false;
+            }
+        },
 
-    { //installation
-        type: "input",
-        name: "installation",
-        message: "Enter instiallation instructions:"
-    },
+        { //usage
+            type: "input",
+            name: "usage",
+            message: "Enter usage instructions:",
+            validate: (userInput) => {
+                if (userInput) {
+                    return true
+                }
+                return false;
+            }
+        },
 
-    { //usage
-        type: "input",
-        name: "usage",
-        message: "Enter usage instructions:"
-    },
+        { //contributing
+            type: "input",
+            name: "contributing",
+            message: "Enter contribution instructions:",
+            validate: (userInput) => {
+                if (userInput) {
+                    return true
+                }
+                return false;
+            }
+        },
 
-    { //contributing
-        type: "input",
-        name: "contributing",
-        message: "Enter contribution instructions:"
-    },
+        { //tests
+            type: "input",
+            name: "tests",
+            message: "Enter testing instructions:",
+            validate: (userInput) => {
+                if (userInput) {
+                    return true
+                }
+                return false;
+            }
+        },
 
-    { //tests
-        type: "input",
-        name: "tests",
-        message: "Enter testing instructions:"
-    },
+        { //liscense
+            type: "list",
+            name: "liscense",
+            message: "Choose a liscense:",
+            choices: ["MIT", "Creative Commons", "Apache 2.0", "None"]
+        },
 
-    { //liscense
-        type: "list",
-        name: "liscense",
-        message: "Choose a liscense:",
-        choices: ["MIT", "Creative Commons", "Apache 2.0", "None"]
-    },
+        { //username
+            type: "input",
+            name: "username",
+            message: "Enter your GitHub username:",
+            validate: (userInput) => {
+                if (userInput) {
+                    return true
+                }
+                return false;
+            }
+        },
 
-    { //username
-        type: "input",
-        name: "username",
-        message: "Enter your GitHub username:"
-    },
+        { //email
+            type: "input",
+            name: "email",
+            message: "Enter your email address:",
+            validate: (userInput) => {
+                if (userInput) {
+                    return true
+                }
+                return false;
+            }
+        },
 
-    { //email
-        type: "input",
-        name: "email",
-        message: "Enter your email address:"
-    },
-
-];
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
